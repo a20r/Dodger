70962a248fbc15dac47fffcbb88b80095b0edeb2
Alex Wallar <aw204@st-andrews.ac.uk>
2015-01-01 16:54:49 +0000

    awesome sauce

--- src/point.cpp
+++ src/point.cpp
@@ -6,6 +6,13 @@
 Point::Point(double x, double y) {
     this->x = x;
     this->y = y;
+    this->z = 0;
+}
+
+Point::Point(double x, double y, double z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
 }
 
 Point::~Point() {
@@ -20,6 +27,10 @@ double Point::get_y() {
     return this->y;
 }
 
+double Point::get_z() {
+    return this->z;
+}
+
 void Point::set_x(double x) {
     this->x = x;
 }
@@ -28,15 +39,20 @@ void Point::set_y(double y) {
     this->y = y;
 }
 
+void Point::set_z(double z) {
+    this->z = z;
+}
+
 double Point::euclid_dist(Point* other_point) {
 
     double px = pow(this->x - other_point->get_x(), 2);
     double py = pow(this->y - other_point->get_y(), 2);
-    return sqrt(px + py);
+    double pz = pow(this->z - other_point->get_z(), 2);
+    return sqrt(px + py + pz);
 
 }
 
 Point* Point::to_unit_vector() {
-    double mag = this->euclid_dist(new Point(0, 0));
-    return new Point(this->x / mag, this->y / mag);
+    double mag = this->euclid_dist(new Point(0, 0, 0));
+    return new Point(this->x / mag, this->y / mag, this->z / mag);
 }
--- src/point.hpp
+++ src/point.hpp
@@ -3,16 +3,20 @@ class Point {
 
     public:
         Point(double x, double y);
+        Point(double x, double y, double z);
         ~Point();
         double get_x();
         double get_y();
+        double get_z();
         void set_x(double x);
         void set_y(double y);
+        void set_z(double y);
         double euclid_dist(Point* other_point);
         Point* to_unit_vector();
 
     private:
-        int x;
-        int y;
+        double x;
+        double y;
+        double z;
 
 };
