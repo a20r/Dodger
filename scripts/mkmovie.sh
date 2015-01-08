61b6a1fd932ce9a70e1314af85948de1ce08674e
Alex Wallar <aw204@st-andrews.ac.uk>
2015-01-07 21:04:45 -0500

    balls

--- scripts/mkmovie.sh
+++ scripts/mkmovie.sh
@@ -1,12 +1,15 @@
 mkdir $2
 rm $2*.png
 
+OUTPUT_JSON=$1$4.json
+OUTPUT_AVI=$3$4.avi
+
 echo Running search
-bin/$4 > $1$4
+bin/$4 > $OUTPUT_JSON
 
 echo Running animator
-python scripts/run_animator.py $1$4 $2
+python scripts/run_animator.py $OUTPUT_JSON $2
 
 echo Creating video
 mencoder mf://$2*.png -mf w=800:h=600:fps=11:type=png -ovc lavc \
-    -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o $3$4.avi
+    -lavcopts vcodec=mpeg4:mbd=2:trell -oac copy -o $OUTPUT_AVI
--- scripts/run_search.sh
+++ scripts/run_search.sh
@@ -3,4 +3,4 @@ cmake .
 make
 
 bash scripts/mkmovie.sh $1 $2 $3 $4
-python scripts/run_drawer.py $1$4
+python scripts/run_drawer.py $1$4.json
