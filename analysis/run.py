8a2e1be10ffe3d44e6d4e900ca9d3f624ee30360
Alex Wallar <aw204@st-andrews.ac.uk>
2015-03-23 01:19:46 +0000

    More graphs

--- analysis/run.py
+++ analysis/run.py
@@ -70,7 +70,7 @@ def plot_noise_vs(tb, means, stds, yunit):
 def plot_noise_planner(planner, i):
     tb = table.load_csv("data/{}_speed_noise_{}.csv".format(planner, i))
     plt.figure()
-    plot_noise_vs(tb, "mean_min_distance", "std_min_distance", "Meters [m]")
+    plot_noise_vs(tb, "mean_min_distance", "std_min_distance", "Mean Minimum Distance [m]")
     plt.title("{}: Speed v.s. Mean Minimum Distance"
               .format(name_dict[planner]))
     plt.ylim([0, 0.45])
@@ -85,7 +85,7 @@ def plot_noise_planner(planner, i):
 
     plt.figure()
     plot_noise_vs(tb, "mean_avg_min_distance", "std_avg_min_distance",
-                  "Meters [m]")
+                  "Mean Average Minimum Distance [m]")
     plt.title("{}: Speed v.s. Mean Average Minimum Distance"
               .format(name_dict[planner]))
     plt.legend()
@@ -93,18 +93,34 @@ def plot_noise_planner(planner, i):
                 bbox_inches="tight")
 
     plt.figure()
-    plot_noise_vs(tb, "avg_times", "std_times", "Seconds [s]")
+    plot_noise_vs(tb, "avg_times", "std_times", "Time [s]")
     plt.title("{}: Speed v.s. Mean Computational Time"
               .format(name_dict[planner]))
     plt.legend()
     plt.savefig("figures/{}_mean_times_{}.pdf".format(planner, i),
                 bbox_inches="tight")
 
+    plt.figure()
+    plot_noise_vs(tb, "mean_max_cost", "std_max_cost", "Mean Maximum Cost")
+    plt.title("{}: Speed v.s. Mean Maximum Planner Cost"
+              .format(name_dict[planner]))
+    plt.legend()
+    plt.savefig("figures/{}_mean_max_cost_{}.pdf".format(planner, i),
+                bbox_inches="tight")
+
+    plt.figure()
+    plot_noise_vs(tb, "mean_avg_cost", "std_avg_cost", "Mean Average Cost")
+    plt.title("{}: Speed v.s. Mean Average Planner Cost"
+              .format(name_dict[planner]))
+    plt.legend()
+    plt.savefig("figures/{}_mean_avg_cost_{}.pdf".format(planner, i),
+                bbox_inches="tight")
+
 
 def plot_noise_std_data(tb, speed, stds, yunit):
     xs = tb("speed", value=speed, s_index="noise")
     ys = tb("speed", value=speed, s_index=stds)
-    plt.xlabel("Meters [m]")
+    plt.xlabel("Obstacle Position Uncertainty [m]")
     plt.ylabel(yunit)
     plt.plot(xs, ys, 'x-', label="Speed = {}".format(speed))
 
@@ -118,7 +134,7 @@ def plot_noise_std_vs(tb, stds, yunit):
 def plot_noise_std_planner(planner, i):
     tb = table.load_csv("data/{}_speed_noise_{}.csv".format(planner, i))
     plt.figure()
-    plot_noise_std_vs(tb, "std_min_distance", "Meters [m]")
+    plot_noise_std_vs(tb, "std_min_distance", "Standard Deviation [m]")
     plt.title("{}: Noise v.s. Mean Minimum Distance Standard Deviation"
               .format(name_dict[planner]))
     plt.legend(loc=2)
@@ -127,7 +143,7 @@ def plot_noise_std_planner(planner, i):
 
     plt.figure()
     plot_noise_std_vs(tb, "std_avg_min_distance",
-                  "Meters [m]")
+                  "Standard Deviation [m]")
     plt.title("{}: Noise v.s. Mean Average Distance Standard Deviation"
               .format(name_dict[planner]))
     plt.legend(loc=2)
@@ -135,17 +151,33 @@ def plot_noise_std_planner(planner, i):
                 bbox_inches="tight")
 
     plt.figure()
-    plot_noise_std_vs(tb, "std_times", "Seconds [s]")
+    plot_noise_std_vs(tb, "std_times", "Standard Deviation [s]")
     plt.title("{}: Noise v.s. Mean Computational Time Standard Deviation"
               .format(name_dict[planner]))
     plt.legend(loc=2)
     plt.savefig("figures/{}_std_avg_times_{}.pdf".format(planner, i),
                 bbox_inches="tight")
 
+    plt.figure()
+    plot_noise_std_vs(tb, "std_max_cost", "Mean Maximum Cost")
+    plt.title("{}: Noise v.s. Mean Maximum Cost Standard Deviation"
+              .format(name_dict[planner]))
+    plt.legend(loc=2)
+    plt.savefig("figures/{}_std_max_cost_{}.pdf".format(planner, i),
+                bbox_inches="tight")
+
+    plt.figure()
+    plot_noise_std_vs(tb, "std_avg_cost", "Mean Average Cost")
+    plt.title("{}: Noise v.s. Mean Average Cost Standard Deviation"
+              .format(name_dict[planner]))
+    plt.legend(loc=2)
+    plt.savefig("figures/{}_std_avg_cost_{}.pdf".format(planner, i),
+                bbox_inches="tight")
+
 
 if __name__ == "__main__":
-    plot_noise_planner("planner", 0)
+    # plot_noise_planner("planner", 0)
     plot_noise_planner("pf", 0)
-    plot_noise_std_planner("planner", 0)
+    # plot_noise_std_planner("planner", 0)
     plot_noise_std_planner("pf", 0)
     # plt.show()
