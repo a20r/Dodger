bcd57987aafeaf98f815a1ed2938a8e2aabcac31
Alex Wallar <aw204@st-andrews.ac.uk>
2015-04-08 18:37:24 +0100

    Wrote equations for last experiment

--- examples/replanner.cpp
+++ examples/replanner.cpp
@@ -30,41 +30,51 @@ int main() {
     std::list<Dodger::Agent *> agents;
 
     Dodger::StochasticAgent *ag = new Dodger::StochasticAgent(
-        new Dodger::CosModel(6, 3, 1, 0),
-        new Dodger::LinearModel(0, 0),
-        Dodger::Point(3.68, 0.5), noise_std);
+        new Dodger::CosModel(-6, 3, 1, 0),
+        new Dodger::CosModel(-6, 3, 1, 0),
+        Dodger::Point(0, 0), noise_std);
 
     Dodger::StochasticAgent *ag2 = new Dodger::StochasticAgent(
-        new Dodger::CosModel(-6, 3, 1, 0),
-        new Dodger::LinearModel(0, 0),
-        Dodger::Point(0.32, 1), noise_std);
-
-    Dodger::StochasticAgent *ag3 = new Dodger::StochasticAgent(
         new Dodger::CosModel(6, 3, 1, 0),
-        new Dodger::LinearModel(0, 0),
-        Dodger::Point(3.68, 1.5), noise_std);
-
-    Dodger::StochasticAgent *ag4 = new Dodger::StochasticAgent(
         new Dodger::CosModel(-6, 3, 1, 0),
-        new Dodger::LinearModel(0, 0),
-        Dodger::Point(0.32, 2), noise_std);
+        Dodger::Point(4, 0), noise_std);
 
-    Dodger::StochasticAgent *ag5 = new Dodger::StochasticAgent(
-        new Dodger::CosModel(6, 3, 1, 0),
-        new Dodger::LinearModel(0, 0),
-        Dodger::Point(3.68, 2.5), noise_std);
+    // Dodger::StochasticAgent *ag = new Dodger::StochasticAgent(
+    //     new Dodger::CosModel(6, 3, 1, 0),
+    //     new Dodger::LinearModel(0, 0),
+    //     Dodger::Point(3.68, 0.5), noise_std);
 
-    Dodger::StochasticAgent *ag6 = new Dodger::StochasticAgent(
-        new Dodger::CosModel(-6, 3, 1, 0),
-        new Dodger::LinearModel(0, 0),
-        Dodger::Point(0.32, 3), noise_std);
+    // Dodger::StochasticAgent *ag2 = new Dodger::StochasticAgent(
+    //     new Dodger::CosModel(-6, 3, 1, 0),
+    //     new Dodger::LinearModel(0, 0),
+    //     Dodger::Point(0.32, 1), noise_std);
+
+    // Dodger::StochasticAgent *ag3 = new Dodger::StochasticAgent(
+    //     new Dodger::CosModel(6, 3, 1, 0),
+    //     new Dodger::LinearModel(0, 0),
+    //     Dodger::Point(3.68, 1.5), noise_std);
+
+    // Dodger::StochasticAgent *ag4 = new Dodger::StochasticAgent(
+    //     new Dodger::CosModel(-6, 3, 1, 0),
+    //     new Dodger::LinearModel(0, 0),
+    //     Dodger::Point(0.32, 2), noise_std);
+
+    // Dodger::StochasticAgent *ag5 = new Dodger::StochasticAgent(
+    //     new Dodger::CosModel(6, 3, 1, 0),
+    //     new Dodger::LinearModel(0, 0),
+    //     Dodger::Point(3.68, 2.5), noise_std);
+
+    // Dodger::StochasticAgent *ag6 = new Dodger::StochasticAgent(
+    //     new Dodger::CosModel(-6, 3, 1, 0),
+    //     new Dodger::LinearModel(0, 0),
+    //     Dodger::Point(0.32, 3), noise_std);
 
     agents.push_back(ag);
     agents.push_back(ag2);
-    agents.push_back(ag3);
-    agents.push_back(ag4);
-    agents.push_back(ag5);
-    agents.push_back(ag6);
+    // agents.push_back(ag3);
+    // agents.push_back(ag4);
+    // agents.push_back(ag5);
+    // agents.push_back(ag6);
 
     Dodger::Point start(2, 0);
     Dodger::Point goal(2, 4);
